68fac91abe9ecba3ea26ba8fb14ce7cb4f5a9aea
David <Rigby335@hotmail.com>
2022-06-13 12:22:36 -0700

    image renders on select option

--- src/City.js
+++ src/City.js
@@ -5,6 +5,7 @@ export default function City(props) {
     <div className="city">
       {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
       {/* use these 3 ids in img tags load approriate images that live in the public directory */}
+      <img src={`/waterfront-${props.waterfrontId}.jpeg`} />
     </div>
   );
 }
